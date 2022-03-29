39929be2468229b60e5bbdbc0bed7c0e50bd9cd4
imoiseeva <moiseeva.irina@gmail.com>
2022-03-29 12:23:35 +0300

    Initial Commit

---
+++ Test assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test assignment
+//
+//  Created by Irina Moiseeva on 29.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
