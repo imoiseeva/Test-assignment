d101f6aba3385318bd98cc64a6a65728803fb83f
imoiseeva <moiseeva.irina@gmail.com>
2022-04-01 13:17:11 +0300

    pods was deintegrated

--- Test assignment/NetworkManager.swift
+++ Test assignment/NetworkManager.swift
@@ -6,7 +6,6 @@
 
 
 import Foundation
-import SwiftSoup
 
 class NetworkManager {
     
@@ -15,28 +14,8 @@ class NetworkManager {
     private init() {}
     
     func fetchInfo() -> String {
-        var title = ""
-        let urlAddress = "https://news.ycombinator.com/"
-        guard let myURL = URL(string: urlAddress) else { return "" }
-        
+        guard let myURL = URL(string: "http://whatismyip.akamai.com") else { return "" }
         let html = try! String(contentsOf: myURL, encoding: .utf8)
-        
-        do {
-            let doc: Document = try SwiftSoup.parseBodyFragment(html)
-            let headerTitle = try doc.title()
-            
-            // my body
-            let body = doc.body()
-            // elements to remove, in this case images
-            let undesiredElements: Elements? = try body?.select("img[src]")
-            //remove
-            try undesiredElements?.remove()
-            title = headerTitle
-            
-            print("Header title: \(headerTitle)")
-        } catch let error {
-            print("Message: \(error)")
-        }
-        return title
+        return html
     }
 }
--- Test assignment/ViewController.swift
+++ Test assignment/ViewController.swift
@@ -9,14 +9,21 @@ import UIKit
 import AudioToolbox
 import AVFoundation
 
-class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate{
+private extension String {
+    static let IPAdress = "getIP"
+    static let flashlight = "toggleFlashLight"
+    static let freezing = "freezing"
+    static let asking = "askMeAnything"
+}
+
+class ViewController: UIViewController{
     
     @IBOutlet weak var imageForAsking: UIImageView!
     @IBOutlet weak var flashlightTextButton: UIButton!
     @IBOutlet weak var IPAddressLabel: UILabel!
     
-    var video = AVCaptureVideoPreviewLayer()
-    let session = AVCaptureSession()
+    private var video = AVCaptureVideoPreviewLayer()
+    private let session = AVCaptureSession()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +31,7 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate{
     }
     
     @IBAction func getIPAddress(_ sender: UIButton) {
-        IPAddressLabel.text = NetworkManager.shared.fetchInfo()
+        fetchIP()
     }
     
     @IBAction func flashlightButton(_ sender: UIButton) {
@@ -36,8 +43,7 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate{
     }
     
     @IBAction func askButton(_ sender: UIButton) {
-        let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-        imageForAsking.image = ballArray[Int.random(in: 0...4)]
+        asking()
     }
     
     @IBAction func scanQR(_ sender: UIButton) {
@@ -46,14 +52,23 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate{
     
     // MARK: - Methods
     
-    func freezing() {
+    private func asking() {
+        let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+        imageForAsking.image = ballArray[Int.random(in: 0...4)]
+    }
+    
+    private func fetchIP() {
+        IPAddressLabel.text = NetworkManager.shared.fetchInfo()
+    }
+    
+    private func freezing() {
         for _ in 1...3 {
             AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
             sleep(1)
         }
     }
     
-    func toggleFlashLight() {
+    private func toggleFlashLight() {
         guard let device = AVCaptureDevice.default(for: AVMediaType.video),
               device.hasTorch else { return }
         do {
@@ -75,30 +90,32 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate{
         }
     }
     
-    func setupVideo() {
+    private func setupVideo() {
+        session.inputs.forEach(self.session.removeInput(_:))
+        session.outputs.forEach(self.session.removeOutput(_:))
         
-        //configure devise
         guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else { return }
-        //input
+        
         do {
             let input = try AVCaptureDeviceInput(device: captureDevice)
-     //       self.session.removeInput(input).. не работает
             session.addInput(input)
         } catch {
             fatalError(error.localizedDescription)
         }
-        //output
+        
         let output = AVCaptureMetadataOutput()
         session.addOutput(output)
         output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)//todo
         output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
         
-        //video
         video = AVCaptureVideoPreviewLayer(session: session)
         video.frame = view.layer.bounds
         view.layer.addSublayer(video)
         session.startRunning()
     }
+}
+
+extension ViewController: AVCaptureMetadataOutputObjectsDelegate{
     
     func metadataOutput(_ captureOutput: AVCaptureMetadataOutput,
                         didOutput metadataObjects: [AVMetadataObject],
@@ -108,25 +125,35 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate{
             return
         }
         
-        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        guard let metadataObj = metadataObjects.first as? AVMetadataMachineReadableCodeObject else { return }
         
-        if metadataObj.type == AVMetadataObject.ObjectType.qr {
-            if let outputString = metadataObj.stringValue {
+        if metadataObj.type == AVMetadataObject.ObjectType.qr, let outputString = metadataObj.stringValue {
+            
+            switch outputString {
+            case .flashlight:
+                self.toggleFlashLight()
+                self.stopCamera()
+            case .freezing:
+                self.freezing()
+                self.stopCamera()
+            case .IPAdress:
                 DispatchQueue.main.async {
-                    print(outputString)
-                    if outputString == "toggleFlashLight" {
-                        self.toggleFlashLight()
-                    }
-                    if outputString == "freezing"{
-                        self.freezing()
-                        self.session.stopRunning()
-                        self.video.removeFromSuperlayer()
-                    }
-                    if outputString == "getIP"{
- //todo - write func
-                    }
+                    self.fetchIP()
+                    self.stopCamera()
                 }
+            case .asking:
+                DispatchQueue.main.async {
+                    self.asking()
+                    self.stopCamera()
+                }
+            default: return
             }
         }
     }
+    
+    func stopCamera(){
+        self.session.stopRunning()
+        self.video.removeFromSuperlayer()
+    }
 }
+
