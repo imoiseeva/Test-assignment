6eb8c4dd0d464c6d615a27ec41fc25b6691914e1
imoiseeva <moiseeva.irina@gmail.com>
2022-04-12 17:10:30 +0300

    The storyboard was deleted

--- Test assignment/ViewController.swift
+++ Test assignment/ViewController.swift
@@ -18,68 +18,216 @@ private extension String {
 
 class ViewController: UIViewController{
     
-    @IBOutlet weak var imageForAsking: UIImageView!
-    @IBOutlet weak var flashlightTextButton: UIButton!
-    @IBOutlet weak var IPAddressLabel: UILabel!
+    private lazy var IPAddressLabel: UILabel = {
+        let lable = UILabel()
+        lable.text = "Your IP Address"
+        lable.font = UIFont.systemFont(ofSize: 15.0)
+        lable.textColor = .gray
+        lable.textAlignment = .left
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        return lable
+    }()
+    
+    private lazy var getIPButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("What's my address?", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 1
+        )
+        button.layer.cornerRadius = 6
+        button.addTarget(self, action: #selector(fetchIP), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var gettingDarkButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("It's getting dark", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 1
+        )
+        button.layer.cornerRadius = 6
+        button.addTarget(self, action: #selector(toggleFlashLight), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var freezingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Brrrr, It's freezing!", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 1
+        )
+        button.layer.cornerRadius = 6
+        button.addTarget(self, action: #selector(freezing), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var askingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Ask Me Anything", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 1
+        )
+        button.layer.cornerRadius = 6
+        button.addTarget(self, action: #selector(asking), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private var imageForAsking: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "Screen")
+        image.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private lazy var QRButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("SCAN QR", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 1
+        )
+        button.layer.cornerRadius = 6
+        button.addTarget(self, action: #selector(setupVideo), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     private var video = AVCaptureVideoPreviewLayer()
     private let session = AVCaptureSession()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        flashlightTextButton.layer.cornerRadius = 6.0
+        view.backgroundColor = .white
+        
+        setupNavigationBar()
+        setupIUElements()
+        setConstraints()
     }
     
-    @IBAction func getIPAddress(_ sender: UIButton) {
-        fetchIP()
+    private func setupNavigationBar() {
+        title = "Test assignment"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarApearence = UINavigationBarAppearance()
+        navBarApearence.configureWithOpaqueBackground()
+        navBarApearence.backgroundColor = UIColor(
+            red: 192/255,
+            green: 192/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        navigationController?.navigationBar.standardAppearance = navBarApearence
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarApearence
+        
+        navigationController?.navigationBar.tintColor = .white
+    }
+    private func setupIUElements() {
+        view.addSubview(IPAddressLabel)
+        view.addSubview(getIPButton)
+        view.addSubview(gettingDarkButton)
+        view.addSubview(freezingButton)
+        view.addSubview(askingButton)
+        view.addSubview(imageForAsking)
+        view.addSubview(QRButton)
+        
     }
     
-    @IBAction func flashlightButton(_ sender: UIButton) {
-        toggleFlashLight()
-    }
-    
-    @IBAction func freezingButton(_ sender: UIButton) {
-        freezing()
-    }
-    
-    @IBAction func askButton(_ sender: UIButton) {
-        asking()
-    }
-    
-    @IBAction func scanQR(_ sender: UIButton) {
-        setupVideo()
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            IPAddressLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            IPAddressLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            IPAddressLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            
+            getIPButton.topAnchor.constraint(equalTo: IPAddressLabel.bottomAnchor, constant: 10),
+            getIPButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            getIPButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            getIPButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            gettingDarkButton.topAnchor.constraint(equalTo: getIPButton.bottomAnchor, constant: 20),
+            gettingDarkButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            gettingDarkButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            gettingDarkButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            freezingButton.topAnchor.constraint(equalTo: gettingDarkButton.bottomAnchor, constant: 20),
+            freezingButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            freezingButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            freezingButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            askingButton.topAnchor.constraint(equalTo: freezingButton.bottomAnchor, constant: 20),
+            askingButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            askingButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            askingButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            imageForAsking.topAnchor.constraint(equalTo: askingButton.bottomAnchor, constant: 10),
+            imageForAsking.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageForAsking.centerYAnchor.constraint(equalTo: view.centerYAnchor , constant: 200),
+            
+            QRButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40),
+            QRButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            QRButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
     }
     
     // MARK: - Methods
     
-    private func asking() {
+    @objc private func asking() {
         let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
         imageForAsking.image = ballArray[Int.random(in: 0...4)]
     }
     
-    private func fetchIP() {
+    @objc private func fetchIP() {
         IPAddressLabel.text = NetworkManager.shared.fetchInfo()
     }
     
-    private func freezing() {
+    @objc func freezing() {
         for _ in 1...3 {
             AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
             sleep(1)
         }
     }
     
-    private func toggleFlashLight() {
+    @objc private func toggleFlashLight() {
         guard let device = AVCaptureDevice.default(for: AVMediaType.video),
               device.hasTorch else { return }
         do {
             try device.lockForConfiguration()
             if device.torchMode == AVCaptureDevice.TorchMode.on {
                 device.torchMode = AVCaptureDevice.TorchMode.off
-                flashlightTextButton.setTitle("It's getting dark", for: .normal)
+                gettingDarkButton.setTitle("It's getting dark", for: .normal)
             } else {
                 do {
                     try device.setTorchModeOn(level: 1.0)
-                    flashlightTextButton.setTitle("You're blinding me", for: .normal)
+                    gettingDarkButton.setTitle("You're blinding me", for: .normal)
                 } catch {
                     print(error)
                 }
@@ -90,7 +238,7 @@ class ViewController: UIViewController{
         }
     }
     
-    private func setupVideo() {
+    @objc private func setupVideo() {
         session.inputs.forEach(self.session.removeInput(_:))
         session.outputs.forEach(self.session.removeOutput(_:))
         
